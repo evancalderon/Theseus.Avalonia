300091020cc587112badd1b762cc2f5650348360
Evan Calderon <evancalderon15@gmail.com>
2022-06-27 15:00:29 -0700

    Search bar implementation, part 2

--- Theseus.Avalonia/Views/MainWindow.axaml.cs
+++ Theseus.Avalonia/Views/MainWindow.axaml.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Globalization;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Data.Converters;
+using Avalonia.Diagnostics;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
 using Avalonia.ReactiveUI;
 using Avalonia.Utilities;
 using ReactiveUI;
@@ -11,14 +16,57 @@ namespace Theseus.Avalonia.Views
 {
     public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
+        private double _searchBorderHeight = 0;
+        
         public MainWindow()
         {
             InitializeComponent();
-            this.WhenActivated(disposable => { });
+            this.WhenActivated(disposable =>
+            {
+            });
+            
+            Opened += delegate
+            {
+                _searchBorderHeight = SearchBorder.Bounds.Height;
+                
+                SearchBox.PropertyChanged += SearchBoxOnPropertyChanged;
+                
+                if (Design.IsDesignMode)
+                    return;
+                ((ExperimentalAcrylicBorder)SearchBox.GetLogicalParent()).Height = _searchBorderHeight - 8;
+                SearchBorder.Height = 0;
+            };
 
 #if DEBUG
             this.AttachDevTools();
 #endif
+        }
+
+        private void SearchBoxOnPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property.Name == "Text")
+            {
+                Console.WriteLine(e.NewValue);
+            }
+        }
+
+        private void ToggleSearchBox(object? sender, PointerPressedEventArgs e)
+        {
+            SearchBorder.SetValue(HeightProperty,
+                SearchBorder.GetValue(HeightProperty) == 0
+                    ? _searchBorderHeight
+                    : 0);
+        }
+
+        private void ShowRealSearch(object? sender, GotFocusEventArgs e)
+        {
+            SearchOverlay.IsVisible = true;
+            SearchBox.Focus();
+        }
+
+        private void HideRealSearch(object? sender, RoutedEventArgs e)
+        {
+            SearchOverlay.IsVisible = false;
         }
     }
 }
